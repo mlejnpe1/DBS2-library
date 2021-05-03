885d4c186eaccf641a0c1fdc2b3886a92000d9ab
Vítězslav Zotov <vzotov42@gmail.com>
2021-05-03 19:59:39 +0200

    Added Filtering

--- src/services/utils.js
+++ src/services/utils.js
@@ -3,7 +3,7 @@ export const formatDate = (d) => {
   date.setUTCHours(2);
   const day = date.getDate();
   const year = date.getFullYear();
-  const hours = date.getHours();
+  const hours = date.getUTCHours();
   const minutes = date.getMinutes();
   const month = date.getMonth() + 1;
   const formattedDate = `${day}.${month}.${year} ${hours}:${minutes}`;
